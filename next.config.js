121cd72efd629f613db237b4280dbbef7265a910
reveleditions <editionsrevel@gmail.com>
2025-10-29 23:09:13 +0100

    New4

--- next.config.js
+++ next.config.js
@@ -6,21 +6,7 @@ const nextConfig = {
     NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL,
   },
 
-  // --- AJOUT DE CE BLOC ---
-  i18n: {
-    /**
-     * Toutes les langues que votre site supportera.
-     * L'erreur était sur /en, donc nous devons inclure 'en'.
-     */
-    locales: ['en', 'fr'], // J'ai ajouté 'fr' au cas où
 
-    /**
-     * La langue par défaut. 
-     * Quand un utilisateur visite "/", c'est la langue qui sera utilisée.
-     */
-    defaultLocale: 'en',
-  },
-  // --- FIN DU BLOC AJOUTÉ ---
 };
 
 module.exports = nextConfig;
